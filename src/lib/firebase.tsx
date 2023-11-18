1b32d8c9a9634e1204ca920184a8a43a4558c32a
Alper <alperusel25@gmail.com>
2023-11-19 00:25:36 +1100

    firebase error

--- src/lib/firebase.tsx
+++ src/lib/firebase.tsx
@@ -3,12 +3,10 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 
-if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
-  });
-}
+firebase.initializeApp({
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+});
 
 export default firebase;

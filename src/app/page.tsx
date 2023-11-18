1f705e767210d23788a6b6dc19392144f6f71643
Alper <alperusel25@gmail.com>
2023-11-18 23:30:47 +1100

    github auth completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import Image from 'next/image'
+'use client'
+
 import styles from './page.module.css'
+import { useAuth } from '@/lib/auth'
 
 export default function Home() {
+  const auth: any = useAuth();
+
   return (
     <main className={styles.main}>
       <h1>Fast Feedback </h1>
+      <button onClick={() => auth.signInWithGithub()}>Sign In</button>
+      <div>{auth?.user?.email}</div>
+      <button onClick={() => auth.signOut()}>Sign Out</button>
     </main>
   )
 }

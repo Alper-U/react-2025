4336b8245942f8e422045ca688a1d43a6be0c41e
Alper <alperusel25@gmail.com>
2023-11-22 00:01:39 +1100

    removed route

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,12 +1,12 @@
-import firestore from "@/lib/firebase-admin";
+// import firestore from "@/lib/firebase-admin";
 
-export async function GET() {
-  const sitesRef = firestore.collection("sites");
-  const doc = await sitesRef.get();
-  if (!doc.exists) {
-    console.log("No such document!");
-  } else {
-    console.log("Document data:", doc.data());
-    return Response.json(doc.data());
-  }
-}
+// export async function GET() {
+//   const sitesRef = firestore.collection("sites");
+//   const doc = await sitesRef.get();
+//   if (!doc.exists) {
+//     console.log("No such document!");
+//   } else {
+//     console.log("Document data:", doc.data());
+//     return Response.json(doc.data());
+//   }
+// }

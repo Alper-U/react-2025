b70bcf63bb77fd73ae990e9845bf9e4eae0e05db
Alper <alperusel25@gmail.com>
2023-11-22 00:03:00 +1100

    stub route

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,12 +1,14 @@
 // import firestore from "@/lib/firebase-admin";
 
-// export async function GET() {
-//   const sitesRef = firestore.collection("sites");
-//   const doc = await sitesRef.get();
-//   if (!doc.exists) {
-//     console.log("No such document!");
-//   } else {
-//     console.log("Document data:", doc.data());
-//     return Response.json(doc.data());
-//   }
-// }
+export async function GET() {
+  // const sitesRef = firestore.collection("sites");
+  // const doc = await sitesRef.get();
+  // if (!doc.exists) {
+  //   console.log("No such document!");
+  // } else {
+  //   console.log("Document data:", doc.data());
+  //   return Response.json(doc.data());
+  // }
+
+  return Response.json({ nextjs: true });
+}
